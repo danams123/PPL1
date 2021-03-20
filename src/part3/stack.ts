e0409993677acbc780883fc7432d612e946528ea
danam <dan.am5555555@gmail.com>
2021-03-20 13:05:09 +0200

    part 3 - state, stack, queue

--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -1,9 +1,22 @@
 import { State, bind } from "./state";
+import * as R from "ramda";
 
 export type Stack = number[];
 
-export const push = undefined;
+export const push: (x: number) => State<Stack, number> = (x) => {
+    return (stack: Stack) => {
+        const nStack: Stack = R.append(x, stack);
 
-export const pop = undefined;
+        return [nStack,];
+    }
+}
 
-export const stackManip = undefined;
+export const pop: (stack: Stack) => [Stack, number] = (stack) => {
+    const toDel = stack[0];
+    const nStack = R.drop(0, stack);
+    return [nStack, toDel];
+}
+
+export const stackManip: (stack: Stack) => [Stack, number] = (stack) => {
+    return bind(pop, (x: number) => bind(push(x * x), () => bind(pop, (y: number) => push(x + y))))(stack);
+}
